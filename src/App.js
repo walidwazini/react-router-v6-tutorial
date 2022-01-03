960a1169c0ac7884b08b561269dff962e99d109e
Walid Wazini <wazini25@gmail.com>
2022-01-03 11:37:42 +0800

    change declaration of routes and custom component in routes

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { Route, Switch, 
+  Routes,
+} from 'react-router-dom';
+
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import MainHeader from './components/MainHeader';
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Routes>
+          <Route path='/welcome' element={<Welcome />} >
+            {/* <Welcome /> */}
+          </Route>
+          <Route path='/products' element={<Products />} exact>
+            {/* <Products /> */}
+          </Route>
+          <Route path='/products/:productId' element={<ProductDetail />} >
+            {/* <ProductDetail /> */}
+          </Route>
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Products Component
+// our-domain.com/product-detail/a-book
